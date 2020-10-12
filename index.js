c8bf477ad4318547fc8afd0cacfd60714789db97
phuc hung <nguyenphuchung09@gmail.com>
2020-10-13 00:22:05 +0700

    version2

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 var express = require('express')
+var shortid = require('shortid');
+var cookieParser = require('cookie-parser')
 var app = express()
 var port = 3000;
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+var userRoute = require('./route/user_route')
+var productRoute = require('./route/product_route')
+var cartRoute = require('./route/cart_route');
+const bodyParser = require('body-parser')
+
+const { sql, createPool } = require('slonik');
+const { connection } = require('pg');
+const { render } = require('ejs');
+const pool = createPool('postgres://postgres:12394nph@localhost:5432/project_ecommerce');
+
+app.set('view engine', 'ejs');
+app.set('views', './view/browser')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static('view/browser'));
+app.use(cookieParser())
+app.use('/account', userRoute);
+app.use('/product', productRoute);
+app.use('/cart', cartRoute);
+
+
+app.get('/home', (req, res) => {
+  if(req.cookies.user_id){
+  pool.connect(async (connection) => {
+    const result = await connection.query(sql`select user_name from public.user where user_id = ${req.cookies.user_id}`);
+    res.render('index', {
+      userId: result.rows[0].user_name,
+    })
+  })}else{
+    res.render('index',{
+      userId:"",
+    })
+  }
+});
+
+app.get('/logOut', (req, res) => {
+  res.clearCookie('user_id');
+  console.log('cookie`s deleted')
+  res.redirect('/home')
 })
- 
-app.listen(port,(req,res)=>{
-    console.log('connect sucessfully, lisening to '+port);
+
+
+app.listen(port, (req, res) => {
+  console.log('connect sucessfully, lisening to ' + port);
 })
